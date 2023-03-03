b902c89106384c9ad9c876356d4c15453b6fc999
Emir Abraham <rosarino74@gmail.com>
2023-03-03 17:02:14 -0300

    asd

--- src/controllers/getUser.js
+++ src/controllers/getUser.js
@@ -4,6 +4,7 @@ const getUser = async (req, res) => {
     const id = req.params.id
     const user = await User.findAll()
     const theUser = await user.find(u => u.id == id)
+    console.log("hola");
     try {
         res.status(200).json(theUser)
     } catch (error) {
